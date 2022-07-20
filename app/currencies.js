843ffafa9843d7344251ffe8daab6f0d272d7a37
alekcangp <40195312+alekcangp@users.noreply.github.com>
2022-07-20 07:51:46 +0600

    Update currencies.js

--- app/currencies.js
+++ app/currencies.js
@@ -1,8 +1,8 @@
 global.currencyTypes = {
 	"btc": {
-		id: "btc",
+		id: "bgl",
 		type:"native",
-		name:"BTC",
+		name:"BGL",
 		multiplier:1,
 		default:true,
 		decimalPlaces:8
@@ -41,7 +41,7 @@ global.currencyTypes = {
 };
 
 global.currencySymbols = {
-	"btc": "₿",
+	"bgl": "bgl",
 	"usd": "$",
 	"eur": "€",
 	"gbp": "£"
